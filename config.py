b8adeddf32c56baf94b1f5e5febba9c73458ac1f
D.P.N.P. Altorf <daniel.altorf@hva.nl>
2020-10-29 15:36:44 +0100

    Adds inits for packages and gitignore

---
+++ config.py
@@ -0,0 +1,4 @@
+import os
+
+BASE_URL = "htttp://www.booking.com"
+HOTEL_URL = "https://www.booking.com/hotel/index.en-gb.html?label=gen173nr-1DCAEoggI46AdIM1gEaKkBiAEBmAEJuAEHyAEM2AED6AEBiAIBqAIDuALD-er8BcACAdICJDg3NWI5YmI4LTdmN2YtNGIyNC1hMmI5LTExMjgyMTA5MmRmNtgCBOACAQ;sid=a3d850b2621de6ff2e3e9ca9e14f9401"
